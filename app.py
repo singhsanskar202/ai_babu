2310c845fc76e5c77c16f6b526f377fe0664722b
singhsanskar202 <72119253+singhsanskar202@users.noreply.github.com>
2025-10-22 20:38:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os, tempfile
 # ------------------------------
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
-    api_key=OPENROUTER_API_KEY,
+    api_key=os.getenv("OPENROUTER_API_KEY"),
 )
 
 def get_engine():
